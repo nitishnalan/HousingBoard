0d764706bec85d785726152843f4f3f47cd37ff7
nitishnalan <nxl170130@gmail.com>
2018-09-29 19:16:51 -0500

    Search Ads Controller, JSPs and DaoImpl

--- src/db/MyDB.java
+++ src/db/MyDB.java
@@ -11,7 +11,8 @@ public interface MyDB {
 
 	String USER="root";
 	String PASS="root";
-	String CONN_URL="jdbc:mysql://127.0.0.1:3306/coursedatabase";
+//	String CONN_URL="jdbc:mysql://127.0.0.1:3306/coursedatabase";
+	String CONN_URL="jdbc:mysql://127.0.0.1:3306/housingboard";
 	
 	
 }
