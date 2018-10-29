999bda7fffaf85f352ceb8395a4cfe557fa06a55
Tushar Chemburkar <chemburkartushar@gmail.com>
2018-10-29 15:16:17 -0500

    Junit for login added

--- src/com/housingboard/junittests/LoginTest.java
+++ src/com/housingboard/junittests/LoginTest.java
@@ -1,17 +1,20 @@
 package com.housingboard.junittests;
-import static net.sourceforge.jwebunit.junit.JWebUnit.assertLinkPresent;
-import static net.sourceforge.jwebunit.junit.JWebUnit.assertTitleEquals;
-import static net.sourceforge.jwebunit.junit.JWebUnit.beginAt;
-import static net.sourceforge.jwebunit.junit.JWebUnit.clickLink;
-import static net.sourceforge.jwebunit.junit.JWebUnit.setBaseUrl;
-import static net.sourceforge.jwebunit.junit.JWebUnit.setTestingEngineKey;
-import org.junit.Test;
+import static net.sourceforge.jwebunit.junit.JWebUnit.*;
+import org.junit.*;
+import net.sourceforge.jwebunit.util.TestingEngineRegistry;
 
 public class LoginTest {
-
+	@Before
+	public void prepare() {
+	     setTestingEngineKey(TestingEngineRegistry.TESTING_ENGINE_HTMLUNIT); 
+	     setBaseUrl("http://localhost:8080/HousingBoard");
+	}
 	@Test
 	public void testLoginPage() {
-		beginAt("login.jsp"); 
-	    assertTitleEquals("HousingBoard");
+		beginAt("login.jsp");
+	    assertTitleEquals("HousingBoard Login");
+	    assertLinkPresent("register");
+	    clickLink("register");
+	    assertTitleEquals("HousingBoard Registration");
 	}
 }
